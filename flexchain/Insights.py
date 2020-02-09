429233493b9d764a32ea96da1f46b201b546be58
gracegreene <gracemargol@.com>
2020-02-09 02:02:28 -0500

    Added Insights file

--- flexchain/Insights.py
+++ flexchain/Insights.py
@@ -1,53 +1,15 @@
-def calc_itr(sku,location,month):
+from flask import (
+    Blueprint, render_template
+)
+
+bp = Blueprint('insight', __name__, url_prefix="/insight")
 
 
-    return itr
+@bp.route('/')
+def get_insight_page():
+    return render_template("ask.html")
 
 
-def sellwhat():
-
-    for sku in products:
-        if getstock_status(sku) < 2:
-            prod_itr[sku] = calc_itr(sku)
-
-    return top 5 of prod_itr[sku]
-#Sort items according to inventory turnover ratio (COGS/((Beginning inventory+Ending inventory)/2)
-#Check if inventory is not in critical level starting from the items with higher inventory turnover ratio
-#Recommend top 5 items to sell, provide explanation on why you are recommending it.
-
-
-    return sku,itr
-
-def sellwhere():
-
-#Input: 2 store locations, 1 SKU, number of items to sell
-#Calculate itr of SKU in both location
-#Calculate shipping cost from warehouse to both locations
-#If one location is both cheaper to ship and has higher ITR, provide a solid recommendation on that location,
- if not then provide insights into itr and shipping cost.
-
-    return recommended location, (location: itr,shipping cost)
-
-def shouldsell():
-
-    if profit > 0:
-        return true
-    else
-        return false
-
-def orderwhere():
-
-    return option
-
-
-def orderquantity(sku,leadtime):
-
-#Fetch current inventory
-#Calculate reorder point using lead time of supplier
-#If current inventory < reorder point then calculate Q
-
-    return orderquantity
-
-def orderwhen():
-
-    return orderdate
+@bp.route('stock-level')
+def get_stock_level_page():
+    return render_template("stock-levels.html")
