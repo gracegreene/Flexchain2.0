28c9ff0ab97446ec24bfa78bda6e672c6556452f
gracegreene <gracemargol@.com>
2020-03-24 21:14:07 -0400

    Created sql for generating 2nd chart data.

--- flexchain/models/chart_data.py
+++ flexchain/models/chart_data.py
@@ -1,6 +1,4 @@
-
-
-
+from ..forecast import forecast
 
 
 def get_txn_amount(cursor):
@@ -18,3 +16,18 @@ def get_txn_amount(cursor):
             'total': total
         })
     return amounts
+
+def get_forecast_inventory(connection, cursor):
+    data = list()
+    sql = '''SELECT prod_name as product,p.sku AS SKU, SUM(quantity) AS inventory
+            FROM current_inventory
+            JOIN product p on current_inventory.sku = p.sku
+            GROUP BY p.sku;'''
+    cursor.execute(sql)
+    for product, inventory in cursor:
+        data.append({
+            'product':product,
+            'forecast':forecast(4,1,0,1,connection),
+            'current_inventory':inventory
+        })
+    return data
